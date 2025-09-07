1751a789f7cb8b9e04f3bee16250693d9b7f3115
Koihin88 <monkeylikeaboy@gmail.com>
2025-09-07 15:18:29 +0800

    Initital commit

---
+++ ios-app/Race-Savant/Race-Savant/Race_SavantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Race_SavantApp.swift
+//  Race-Savant
+//
+//  Created by Koihin Wong on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Race_SavantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
